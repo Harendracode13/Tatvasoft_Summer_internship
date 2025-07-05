c14759cc22982c0aa015290f7eea5302b16ce359
Harendra Piprotar <146612793+Harendracode13@users.noreply.github.com>
2025-07-05 19:50:46 +0530

    internship task 3

--- Day 2/first_app/src/app/user-form/user-form.component.ts
+++ Day 2/first_app/src/app/user-form/user-form.component.ts
@@ -2,10 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
   templateUrl: './user-form.component.html',
-  styleUrl: './user-form.component.css'
+  styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
+  user = {
+    name: '',
+    email: '',
+    role: ''
+  };
 
+  submitted = false;
+
+  roles = ['Admin', 'Editor', 'Viewer'];
+
+  onSubmit() {
+    this.submitted = true;
+  }
 }
